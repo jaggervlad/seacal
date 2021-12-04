08bbc58e70a7be03c905a798924c4ed27191d859
Sebastian Acosta Alonso <45018754+jaggervlad@users.noreply.github.com>
2021-12-03 19:47:00 -0500

    Adding contentlayer and preact compilation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
+const { withContentlayer } = require('next-contentlayer');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withContentlayer()({
+  swcMinify: true,
   reactStrictMode: true,
-};
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/text-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  },
+});
