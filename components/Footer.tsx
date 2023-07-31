1dc540ebcb53a2c0eabe6cc42acc43ac8cd68d74
Sebastian Acosta Alonso <45018754+jaggervlad@users.noreply.github.com>
2023-07-31 11:28:23 -0500

    Change py footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <p className="text-sm py-2">© {new Date().getFullYear()} seacal Todos los derechos reservados.</p>;
+  return <p className="text-sm py-4">© {new Date().getFullYear()} seacal Todos los derechos reservados.</p>;
 };
 
 export default Footer;
